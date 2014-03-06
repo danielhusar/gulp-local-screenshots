5d04f362d9b857fdeafc40753fd53d3394e2b8a9
Daniel Husar <dano.husar@gmail.com>
2014-03-06 17:06:51 +0000

    test fix

--- test.js
+++ test.js
@@ -15,8 +15,8 @@ it('It should generate properly screenshot', function (cb) {
   stream.on('end', function(){
     console.log('\r\n');
     dimensions = sizeOf('screens/index-1024.jpg');
-    assert.equal(1024, 1024);
-    assert.equal(367, 367);
+    assert.equal(dimensions.width, 1024);
+    assert.equal(dimensions.height, 367);
     fs.unlinkSync('screens/index-1024.jpg');
     fs.rmdirSync('screens');
     cb();
